0940bb9ba5b3898111e695ae91352df1c5bcd5a6
Ali Gamal <hictors@gmail.com>
2021-08-16 11:48:17 +0200

    passwod_generator2

--- importing_local_script/password_generator.py
+++ importing_local_script/password_generator.py
@@ -1,9 +1,9 @@
-import random
-import string
+from random import choice , sample
+from string import ascii_letters , digits , punctuation
 
-letters = string.ascii_letters
-numbers = string.digits
-special_characters = string.punctuation
+letters = ascii_letters
+numbers = digits
+special_characters = punctuation
 
 
 letters_num = int(input('How many letters you want in your Password? '))
@@ -15,16 +15,16 @@ special_num = int(input('How many special Characters you want in your Password? 
 password = ''
 
 for i  in range(letters_num):
-    password+= random.choice(letters)
+    password+= choice(letters)
 
 
 for i  in range(numbers_num):
-    password+= random.choice(numbers)
+    password+= choice(numbers)
 
 
 for i  in range(special_num):
-    password+= random.choice(special_characters)
+    password+= choice(special_characters)
 print(password)
-mix_password = "".join(random.sample(password , len(password)))
+mix_password = "".join(sample(password , len(password)))
 
 print('your Password is :' ,mix_password)
