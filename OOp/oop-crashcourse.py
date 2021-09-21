373aa21245fd99630d1485623c3cfdc6bf01dc2e
Ali Gamal Ali <hictors@gmail.com>
2021-09-21 19:57:52 +0200

    OOP

---
+++ OOp/oop-crashcourse.py
@@ -0,0 +1,8 @@
+# Object Oriented Programming
+# each object has 2 essential parts
+# 1-Characteristics they call it (Attribute)   \|/   2-Actions they call it (Methods)
+# like name                                    \|/     #like sell item
+# address                                      \|/     # take item
+
+# Object VS CLass
+# object is specific like shirts { color : yellow , size : medium , style : short , price : 14}
